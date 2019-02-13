537f5917944810c099851106aa98d69e43489748
Aleksi Sapon <aleksi@sapon.ca>
2019-02-13 13:18:18 -0500

    Working DUE version

--- Interface.ino
+++ Interface.ino
@@ -1,79 +1,70 @@
-const unsigned int pinSC = 2;
-const unsigned int pinSI = 4;
-const unsigned int pinSD = 3;
-
-const unsigned long baud3 = 115200L;
+const unsigned int pinSC = 25;
+const unsigned int pinSI = 26;
+const unsigned int pinSD = 27;
 
 void setup() {
-  Serial.begin(baud3);
+  pmc_set_writeprotect(false);
+
+  pmc_enable_periph_clk(ID_TC0);
+
+  NVIC_EnableIRQ(TC0_IRQn);
+  NVIC_EnableIRQ(PIOD_IRQn);
+
+  Serial.begin(250000);
+
   pinMode(pinSC, INPUT_PULLUP);
   pinMode(pinSI, INPUT_PULLUP);
   pinMode(pinSD, INPUT_PULLUP);
-  
-  attachInterrupt(digitalPinToInterrupt(pinSC), signalTransfer, CHANGE);
+
   attachInterrupt(digitalPinToInterrupt(pinSD), startRead, FALLING);
 }
 
-bool transfering = false;
-/*bool reading = false;
 unsigned int readCountSD = 0;
 unsigned int bitsSD = 0;
 
-void startTimer0Baud3() { 
-  TCCR0A = 1 << WGM01; // CTC mode
-  TCCR0B = 1 << CS00; // 1 prescaler 
-  OCR0A = 138; // 16MHz / (baud3 * prescaler) - 1
-  TCNT0 = 0; // clear counter value
-  
-  TIMSK0 |= 1 << OCIE0A;
+void startTimer0Baud3() {
+  // Timer clock1 (42MHz)
+  REG_TC0_CMR0 = TC_CMR_TCCLKS_TIMER_CLOCK1 | TC_CMR_CPCTRG;
+
+  // Enabel RC compare interrupt
+  REG_TC0_IER0 = TC_IER_CPCS;
+
+  //Set RC compare value
+  REG_TC0_RC0 = 365; // round(42 MHz / 115 200 pbs)
+
+  //Start timer
+  REG_TC0_CCR0 = TC_CCR_SWTRG | TC_CCR_CLKEN;
 }
 
 void stopTimer0() {
-  TIMSK0 &= ~(1 << OCIE0A);
+  REG_TC0_CCR0 = TC_CCR_CLKDIS;
 }
 
-ISR(TIMER0_COMPA_vect) {  
+void TC0_Handler() {
+   // Clear timer 0 interrupt status
+  (void)REG_TC0_SR0;
   if (readCountSD >= 16) {
-    reading = false;
     stopTimer0();
+    // Clear port D interrupt status
+    (void)REG_PIOD_ISR;
+    NVIC_EnableIRQ(PIOD_IRQn);
     Serial.println(bitsSD, HEX);
   } else {
-    unsigned int bitSD = (PIND >> pinSD) & 1;
+    unsigned int bitSD = digitalRead(pinSD);
     bitsSD |= bitSD << readCountSD;
   }
-  
+
   readCountSD++;
-}*/
-
-void signalTransfer() {
-  transfering = !((PIND >> pinSC) & 1);
 }
-
-#define NOP __asm__ __volatile__ ("nop\n\t")
 
 void startRead() {
-  if (transfering) {
-    unsigned int test = 0;
-
-    for (int i = 0; i < 16; i++) {
-      NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;
-      NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;
-      NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;//NOP;NOP;NOP;NOP;
-      
-      unsigned int bitSD = (PIND >> pinSD) & 1;
-      test |= bitSD << i;
-    }
-
-    Serial.println(test, HEX);
-  }
-  /*if (transfering && !reading) {
-    reading = true;
+  if (!digitalRead(pinSC)) {
     readCountSD = 0;
     bitsSD = 0;
-    startTimer0Baud3(); 
-  }*/
+    NVIC_DisableIRQ(PIOD_IRQn);
+    startTimer0Baud3();
+  }
 }
 
-//3ede
 void loop() {
 }
