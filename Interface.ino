90bb98d3590ec22343e7bfcd8deaa6ec2c336bbc
Aleksi Sapon <aleksi@sapon.ca>
2019-02-13 20:53:30 -0500

    Working read and write to GBA

--- Interface.ino
+++ Interface.ino
@@ -3,67 +3,105 @@ const unsigned int pinSI = 26;
 const unsigned int pinSD = 27;
 
 void setup() {
-  pmc_set_writeprotect(false);
+    pmc_set_writeprotect(false);
 
-  pmc_enable_periph_clk(ID_TC0);
+    pmc_enable_periph_clk(ID_TC0);
 
-  NVIC_EnableIRQ(TC0_IRQn);
-  NVIC_EnableIRQ(PIOD_IRQn);
+    NVIC_EnableIRQ(TC0_IRQn);
+    NVIC_EnableIRQ(PIOD_IRQn);
 
-  Serial.begin(250000);
+    Serial.begin(250000);
 
-  pinMode(pinSC, INPUT_PULLUP);
-  pinMode(pinSI, INPUT_PULLUP);
-  pinMode(pinSD, INPUT_PULLUP);
+    pinMode(pinSC, INPUT_PULLUP);
+    pinMode(pinSI, INPUT_PULLUP);
+    pinMode(pinSD, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(pinSD), startRead, FALLING);
+    attachInterrupt(digitalPinToInterrupt(pinSD), startRead, FALLING);
+    attachInterrupt(digitalPinToInterrupt(pinSI), startWrite, FALLING);
 }
 
-unsigned int readCountSD = 0;
-unsigned int bitsSD = 0;
+unsigned int dataIn = 0;
+unsigned int dataOut = 0xB9A0;
 
-void startTimer0Baud3() {
-  // Timer clock1 (42MHz)
-  REG_TC0_CMR0 = TC_CMR_TCCLKS_TIMER_CLOCK1 | TC_CMR_CPCTRG;
+unsigned int bitCount = 0;
 
-  // Enabel RC compare interrupt
-  REG_TC0_IER0 = TC_IER_CPCS;
+bool writing = false;
 
-  //Set RC compare value
-  REG_TC0_RC0 = 365; // round(42 MHz / 115 200 pbs)
+void startTimerBaud3() {
+    // Timer clock1 (42MHz)
+    REG_TC0_CMR0 = TC_CMR_TCCLKS_TIMER_CLOCK1 | TC_CMR_CPCTRG;
 
-  //Start timer
-  REG_TC0_CCR0 = TC_CCR_SWTRG | TC_CCR_CLKEN;
+    // Enabel RC compare interrupt
+    REG_TC0_IER0 = TC_IER_CPCS;
+
+    //Set RC compare value
+    REG_TC0_RC0 = 365; // round(42 MHz / 115 200 pbs)
+
+    //Start timer
+    REG_TC0_CCR0 = TC_CCR_SWTRG | TC_CCR_CLKEN;
 }
 
-void stopTimer0() {
-  REG_TC0_CCR0 = TC_CCR_CLKDIS;
+void stopTimerBaud3() {
+    REG_TC0_CCR0 = TC_CCR_CLKDIS;
 }
 
 void TC0_Handler() {
-   // Clear timer 0 interrupt status
-  (void)REG_TC0_SR0;
-  if (readCountSD >= 16) {
-    stopTimer0();
-    // Clear port D interrupt status
-    (void)REG_PIOD_ISR;
-    NVIC_EnableIRQ(PIOD_IRQn);
-    Serial.println(bitsSD, HEX);
-  } else {
-    unsigned int bitSD = digitalRead(pinSD);
-    bitsSD |= bitSD << readCountSD;
-  }
+     // Clear timer 0 interrupt status
+    (void)REG_TC0_SR0;
 
-  readCountSD++;
+    if (writing) {
+        if (bitCount < 1) {
+            // Start bit
+            pinMode(pinSD, OUTPUT);
+            digitalWrite(pinSD, LOW);
+            bitCount++;
+        } else if (bitCount < 17) {
+            unsigned int bitSD = (dataOut >> (bitCount - 1)) & 1;
+            digitalWrite(pinSD, bitSD ? HIGH : LOW);
+            bitCount++;
+        } else {
+            pinMode(pinSD, INPUT_PULLUP);
+            // Switching to OUTPUT mode removed the interrupt
+            attachInterrupt(digitalPinToInterrupt(pinSD), startRead, FALLING);
+
+            stopTimerBaud3();
+            // Clear port D interrupt status
+            (void)REG_PIOD_ISR;
+            NVIC_EnableIRQ(PIOD_IRQn);
+
+            Serial.println(dataIn, HEX);
+        }
+    } else {
+        if (bitCount >= 16) {
+            stopTimerBaud3();
+            // Clear port D interrupt status
+            (void)REG_PIOD_ISR;
+            NVIC_EnableIRQ(PIOD_IRQn);
+        } else {
+            unsigned int bitSD = digitalRead(pinSD);
+            dataIn |= (bitSD & 1) << bitCount;
+            bitCount++;
+        }
+    }
 }
 
 void startRead() {
-  if (!digitalRead(pinSC)) {
-    readCountSD = 0;
-    bitsSD = 0;
-    NVIC_DisableIRQ(PIOD_IRQn);
-    startTimer0Baud3();
-  }
+    if (!digitalRead(pinSC)) {
+        bitCount = 0;
+        dataIn = 0;
+        writing = false;
+        NVIC_DisableIRQ(PIOD_IRQn);
+        startTimerBaud3();
+    }
+}
+
+void startWrite() {
+    if (!digitalRead(pinSC)) {
+        bitCount = 0;
+        writing = true;
+        NVIC_DisableIRQ(PIOD_IRQn);
+        startTimerBaud3();
+    }
 }
 
 void loop() {
